c71556d8dd2cd7b1103093baa2718054dcd4b727
Gaganpreet Singh Sidhu <c0703141@mylambton.ca>
2019-01-31 12:24:07 -0500

    Final

--- newweek4/js/main.js
+++ newweek4/js/main.js
@@ -2,6 +2,7 @@
 function myFunction() {
 
     var cat = new Date();
+
     document.body.innerHTML = "<h1> Ther date is" + (cat.getMonth() + 1) + "/" + cat.getDate() + "/" + cat.getFullYear() + "</h1>"
 
 }
